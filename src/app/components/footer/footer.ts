9a7b48156592a0f149803fa7d42834165c21472a
Anup Saw <anupsaw@gmail.com>
2019-10-31 00:21:51 -0400

    Added html import

---
+++ src/app/components/footer/footer.ts
@@ -0,0 +1,9 @@
+import { SzBaseElement } from '../../../lib/base-element';
+import template from './footer.html';
+
+export class SzFooter extends SzBaseElement {
+
+    constructor() {
+        super(template);
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,12 @@ import './lib/element-reference';
 import { CustomElement } from './app/components/custom-element/custom-element';
 import { SzCustomForm } from './app/components/custom-form/custom-form';
 import { SzHeader } from './app/components/header/header';
+import { SzFooter } from './app/components/footer/footer';
 
 
 customElements.define('custom-element', CustomElement);
 customElements.define('custom-form', SzCustomForm);
 customElements.define('sz-header', SzHeader);
+customElements.define('sz-footer', SzFooter);
 console.log('Webpack 123');
 
