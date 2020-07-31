5a973715a25c19af9566c1ad26ca4334033160d4
Anup Saw <anupsaw@gmail.com>
2020-07-31 02:05:08 -0400

    AKS-000008: Added url input fields

--- src/app/components/options/options.ts
+++ src/app/components/options/options.ts
@@ -7,20 +7,33 @@ export class SzDemoOptions extends SzBaseElement {
     private data = ['google.com', 'facebook.com', 'twitter.com', 'yahoo.com', 'bottle.com', 'cup.com', 'pipe.com']
     constructor() {
         super(template);
-        (this.querySelector('#close') as HTMLButtonElement).addEventListener('click', () => {
-            console.log('clicked');
-            this.close();
-        });
+        this.addListener('#close', 'click', () => this.close());
+        this.addListener('#optionsAdd', 'click', () => this.addUrl());
+        // (this.querySelector('#close') as HTMLButtonElement).addEventListener('click', () => {
+        //     console.log('clicked');
+        //     this.close();
+        // });
         this.init();
     }
 
     public init(): void {
+        this.querySelector('.sz-options-url-list').innerHTML = '';
         this.data.forEach((val: string) => {
-            this.querySelector('.sz-main-container').appendChild(new SzListComponent(val));
+            this.querySelector('.sz-options-url-list').appendChild(new SzListComponent(val));
 
         })
     }
 
+    public addUrl(): void {
+        const url = (this.querySelector('#site') as HTMLInputElement).value;
+        this.data.push(url);
+        this.init();
+    }
+
     public close(): void { }
+
+    private addListener(selector: string, event: string, listener: () => void): void {
+        this.querySelector(selector).addEventListener(event, listener);
+    }
 
 }
