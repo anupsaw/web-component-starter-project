7534c27a01a06cb00a63bba1a3f3e198ac7b6da3
Anup Saw <anupsaw@gmail.com>
2020-07-30 20:11:17 -0400

    AKS-000005: Added prod webpack config file

---
+++ webpack.config.prod.js
@@ -0,0 +1,59 @@
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPartialsPlugin = require('html-webpack-partials-plugin')
+const path = require('path');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
+
+module.exports = {
+    entry: './src/index.ts',
+    mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader',
+                        options: { minimize: true }
+                    }
+                ]
+            },
+            {
+                test: /\.svg$/,
+                loader: 'svg-inline-loader'
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+        plugins: [
+            new TsconfigPathsPlugin({
+                configFile: path.resolve(__dirname, './tsconfig.json'),
+                extensions: ['.ts', '.tsx', '.js'],
+                logLevel: 'INFO',
+                baseUrl: path.resolve(__dirname, '.'),
+                mainFields: ['browser', 'main'],
+            }),
+        ]
+
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            template: './src/index.html',
+            filename: './index.html'
+        })
+    ]
+}
